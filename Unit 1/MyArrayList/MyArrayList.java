46784065762d1d61acc6c6c922342c9b4b538467
mattin-tasbihgoo <mtasbihgoo1@hwemail.com>
2025-09-03 09:07:59 -0700

    I'll finish the rest at home--added a few methods and abolished comments
    its too early for ts twin ✌️

--- Unit 1/MyArrayList/MyArrayList.java
+++ Unit 1/MyArrayList/MyArrayList.java
@@ -1,67 +1,57 @@
-/* See ArrayList documentation here:
- * http://docs.oracle.com/javase/7/docs/api/java/util/ArrayList.html
- */
+//  * http://docs.oracle.com/javase/7/docs/api/java/util/ArrayList.html
 
- /*
- * Your indexed functions should throw IndexOutOfBoundsException if index is invalid!
- */
+/*
+* Your indexed functions should throw IndexOutOfBoundsException if index is invalid!
+*/
 public class MyArrayList<E> {
 
-    /* Internal Object counter */
     protected int objectCount;
 
-    /* Internal Object array */
     protected E[] internalArray;
 
-    /* Constructor: Create it with whatever capacity you want? */
     @SuppressWarnings("unchecked")
     public MyArrayList() {
         this.internalArray = (E[]) new Object[100];
     }
 
-    /* Constructor with initial capacity */
     @SuppressWarnings("unchecked")
     public MyArrayList(int initialCapacity) {
         this.internalArray = (E[]) new Object[initialCapacity];
     }
 
-    /* Return the number of active slots in the array list */
     public int size() {
         return objectCount;
     }
 
-    /* Are there zero objects in the array list? */
- /*
-	 * public boolean isEmpty() {
-	 * 
-	 * }
-     */
+    public boolean isEmpty() {
+        String string = "";
+        for (E internalArray1 : internalArray) {
+            string += internalArray1;
+        }
+        if (string.equals("")) {
+            return true;
+        }
+        return false;
+    }
 
- /* Get the index-th object in the list. */
     public E get(int index) {
         return internalArray[index];
     }
 
-    /* Replace the object at index with obj. returns object that was replaced. */
     public E set(int index, E obj) {
         E temp = internalArray[index];
         internalArray[index] = obj;
         return temp;
     }
 
-    /*
-	 * Returns true if this list contains an element equal to obj;
-	 * otherwise returns false.
-     */
     public boolean contains(E obj) {
         for (E internalArray1 : internalArray) {
-            if (internalArray1.equals(obj));
+            if (internalArray1.equals(obj))
+                ;
         }
         return false;
     }
 
-    /* Insert an object at index */
-    // @SuppressWarnings("unchecked")
     public void add(int index, E obj) {
         internalArray[index] = obj;
         if (index >= objectCount) {
@@ -69,42 +59,65 @@ public class MyArrayList<E> {
         }
     }
 
-    /* Add an object to the end of the list; returns true */
- /*
-	 * @SuppressWarnings("unchecked")
-	 * public boolean add(E obj) {
-	 * 
-	 * }
-     */
+    // @SuppressWarnings("unchecked")
+    public boolean add(E obj) {
+        objectCount++;
+        internalArray[objectCount] = obj;
+        return true;
+    }
 
- /* Remove the object at index and shift. Returns removed object. */
- /*
-	 * public E remove(int index) {
-	 * 
-	 * }
-     */
+    /* Remove the object at index and shift. Returns removed object. */
 
- /*
-	 * Removes the first occurrence of the specified element from this list,
-	 * if it is present. If the list does not contain the element, it is unchanged.
-	 * More formally, removes the element with the lowest index i such that
-	 * (o==null ? get(i)==null : o.equals(get(i))) (if such an element exists).
-	 * Returns true if this list contained the specified element (or equivalently,
-	 * if this list changed as a result of the call).
-     */
- /*
-	 * public boolean remove(E obj) {
-	 * 
-	 * }
-     */
+    public E remove(int index) {
 
- /*
-	 * For testing; your string should output as "[X, X, X, X, ...]" where X, X, X,
-	 * X, ... are the elements in the ArrayList.
-	 * If the array is empty, it should return "[]". If there is one element, "[X]",
-	 * etc.
-	 * Elements are separated by a comma and a space.
-     */
+        if (index < 0 || index > internalArray.length) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        E r = internalArray[index];
+
+        int num = objectCount - index - 1;
+
+        if (num > 0) {
+            System.arraycopy(internalArray, index + 1, internalArray, index, num);
+        }
+        internalArray[--objectCount] = null;
+        return r;
+    }
+
+    public boolean remove(E obj) {
+        int i = indexOf(obj); // to make
+        if (i >= 0) {
+            remove(i);
+            return true;
+        }
+        return false;
+
+        /*
+         * for (int i = 0; i < objectCount; i++) {
+         * if (internalArray[i].equals(obj)) {
+         * internalArray[i] = null;
+         * return true;
+         * }
+         * }
+         */
+
+        /*
+         * int i = 0;
+         * while (i < objectCount) {
+         * if (obj == null ? get(i) == null : obj.equals(get(i))) {
+         * break;
+         * }
+         * i++;
+         * }
+         * if (i++ == objectCount) {
+         * return true;
+         * }
+         * 
+         * return false;
+         */
+    }
+
     @Override
     public String toString() {
         String string = "";
