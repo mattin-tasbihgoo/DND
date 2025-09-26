e82aaca402c767e0d1d6aa9ba5c7d7f8763fa8eb
Mattin <mtasbihgoo1@hwemail.com>
2025-09-25 22:51:24 -0700

    25 September
    Ok so I finished it and got 100, but need to go thru again for the private testers (?). Also, github is showing it like i deleted my whole file and pasted it fresh because i formatted it bruh. And now i just wasted 2 hours off 5 hours of sleep trying to fix my non broken code, making it broken, and then debugging it bc i thought i could improve it bruh

--- Unit 2/SinglyLinkedList.java
+++ Unit 2/SinglyLinkedList.java
@@ -1,157 +1,212 @@
-// Implements a singly-linked list.
 
 // import java.util.List;
 @SuppressWarnings("unchecked")
 public class SinglyLinkedList<E> {
-	private ListNode<E> head;
-	private ListNode<E> tail;
-	private int nodeCount;
 
-	// Constructor: creates an empty list
-	public SinglyLinkedList() {
-		this.head = null;
-		this.tail = null;
-		this.nodeCount = 0;
-	}
+    private ListNode<E> head;
+    private ListNode<E> tail;
+    private int nodeCount;
 
-	// Constructor: creates a list that contains
-	// all elements from the array values, in the same order
-	public SinglyLinkedList(Object[] values) {
-		for (int i = 0; i < values.length; i++) {
-			add((E) values[i]);
-		}
-	}
+    // Constructor: creates an empty list
+    public SinglyLinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.nodeCount = 0;
+    }
 
-	public ListNode<E> getHead() {
-		return head;
-	}
+    // Constructor: creates a list that contains
+    // all elements from the array values, in the same order
+    @SuppressWarnings("OverridableMethodCallInConstructor")
+    public SinglyLinkedList(Object[] values) {
+        this();
+        for (Object value : values) {
+            add((E) value);
+        }
+    }
 
-	public ListNode<E> getTail() {
-		return tail;
-	}
+    public ListNode<E> getHead() {
+        return head;
+    }
 
-	// Returns true if this list is empty; otherwise returns false.
-	public boolean isEmpty() {
-		if (nodeCount == 0)
-			;
-		return false;
-	}
+    public ListNode<E> getTail() {
+        return tail;
+    }
 
-	// Returns the number of elements in this list.
-	public int size() {
-		return nodeCount;
-	}
+    // Returns true if this list is empty; otherwise returns false.
+    public boolean isEmpty() {
+        return nodeCount == 0;
+    }
 
-	// Returns true if this list contains an element equal to obj;
-	// otherwise returns false.
-	public boolean contains(E obj) {
-		ListNode<E> temp = head;
-		for (int i = 0; i < nodeCount; i++) {
-			if (temp.getNext().equals(obj)) {
-				return true;
-			}
-			temp = temp.getNext();
-		}
-		return false;
-	}
+    // Returns the number of elements in this list.
+    public int size() {
+        return nodeCount;
+    }
 
-	// Returns the index of the first element in equal to obj;
-	// if not found, returns -1.
-	public int indexOf(E obj) {
-		ListNode<E> temp = head;
-		for (int i = 0; i < nodeCount; i++) {
-			if (temp.getValue().equals(obj)) {
-				return i;
-			}
-			temp = temp.getNext();
-		}
-		return -1;
-	}
+    // Returns true if this list contains an element equal to obj;
+    // otherwise returns false.
+    public boolean contains(E obj) {
+        ListNode<E> temp = head;
+        for (int i = 0; i < nodeCount; i++) {
+            if (temp.getValue().equals(obj)) {
+                return true;
+            }
+            temp = temp.getNext();
+        }
+        return false;
+    }
 
-	// Adds obj to this collection. Returns true if successful;
-	// otherwise returns false.
-	// figure out the head
-	public boolean add(E obj) {
-		if (nodeCount == 0) {
-			head = new ListNode<E>(obj, tail);
-			tail = head;
-		} else {
-			ListNode<E> node = new ListNode<E>(obj);
-			tail.setNext(node);
-			tail = node;
-			nodeCount++;
-		}
-		return true;
-	}
+    // Returns the index of the first element in equal to obj;
+    // if not found, returns -1.
+    public int indexOf(E obj) {
+        ListNode<E> temp = head;
+        for (int i = 0; i < nodeCount; i++) {
+            if (temp.getValue().equals(obj)) {
+                return i;
+            }
+            temp = temp.getNext();
+        }
+        return -1;
+    }
 
-	// Removes the first element that is equal to obj, if any.
-	// Returns true if successful; otherwise returns false.
-	public boolean remove(E obj) {
-		int i = indexOf(obj);
-		ListNode<E> temp = forHelper(0, i - 1);
-		ListNode<E> temp2 = temp.getNext();
-		temp2 = temp2.getNext();
-		temp.setNext(temp2);
-		nodeCount--;
-		return true;
-	}
+    // Adds obj to this collection. Returns true if successful;
+    // otherwise returns false.
+    // figure out the head
+    public boolean add(E obj) {
+        ListNode<E> temp = new ListNode<>(obj);
+        if (head == null) {
+            head = temp;
+            tail = head;
+        } else {
+            tail.setNext(temp);
+            tail = temp;
+        }
+        nodeCount++;
+        return true;
+    }
 
-	// Returns the i-th element.
+    // Removes the first element that is equal to obj, if any.
+    // Returns true if successful; otherwise returns false.
+    public boolean remove(E obj) {
+        if (head == null) {
+            return false;
+        }
+        ListNode<E> temp = head;
 
-	public E get(int i) {
-		return (E) forHelper(0, i).getValue();
-	}
+        if (head.getValue().equals(obj)) {
+            head = head.getNext();
+            nodeCount--;
+            if (head == null) {
+                tail = null;
+            }
+            return true;
+        }
 
-	// Replaces the i-th element with obj and returns the old value.
-	public E set(int i, Object obj) {
+        while (temp.getNext() != null) {
+            if (temp.getNext().getValue().equals(obj)) {
+                if (temp.getNext() == tail) {
+                    tail = temp;
+                }
+                temp.setNext(temp.getNext().getNext());
+                nodeCount--;
+                return true;
+            }
+            temp = temp.getNext();
+        }
+        return false;
+    }
 
-	}
+    // Returns the i-th element.
+    public E get(int i) {
+        if (i < 0 || i >= nodeCount) {
+            throw new IndexOutOfBoundsException();
+        }
+        return forHelper(0, i).getValue();
+    }
 
-	// Inserts obj to become the i-th element. Increments the size
-	// of the list by one.
-	public void add(int i, Object obj) {
-		if (i == 0) {
-			head = new ListNode<E>((E) obj, tail);
-			tail = head;
-		} else {
-			ListNode<E> node = new ListNode<E>((E) obj);
-			tail.setNext(node);
-			tail = node;
-			nodeCount++;
-		}
-	}
+    // Replaces the i-th element with obj and returns the old value.
+    public E set(int i, Object obj) {
+        ListNode<E> temp = forHelper(0, i);
+        E temp2 = temp.getValue();
+        temp.setValue((E) obj);
+        return temp2;
+    }
 
-	// Removes the i-th element and returns its value.
-	// Decrements the size of the list by one.
-	public E remove(int i) {
-		ListNode<E> temp = forHelper(0, i - 1);
-		ListNode<E> temp2 = temp.getNext();
-		ListNode<E> temp3 = temp2.getNext();
-		temp.setNext(temp3);
-		nodeCount--;
-		return (E) temp2;
-	}
+    // Inserts obj to become the i-th element. Increments the size
+    // of the list by one.
+    public void add(int i, Object obj) {
+        if (i < 0 || i > nodeCount) {
+            throw new IndexOutOfBoundsException();
+        }
+        ListNode<E> temp = new ListNode<>((E) obj);
+        if (i == 0) {
+            temp.setNext(head);
+            head = temp;
+            if (nodeCount == 0) {
+                tail = temp;
+            }
+        } else {
+            ListNode<E> temp2 = forHelper(0, i - 1);
+            temp.setNext(temp2.getNext());
+            temp2.setNext(temp);
+            if (temp.getNext() == null) {
+                tail = temp;
+            }
+        }
+        nodeCount++;
+    }
 
-	// Returns a string representation of this list exactly like that for
-	// MyArrayList.
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("[");
-		E temp = (E) getHead();
-		sb.append(temp);
-		while (((ListNode<E>) temp).getNext() != null) {
-			sb.append(((ListNode<E>) temp).getNext());
-			sb.append(",");
-		}
-		sb.append("]");
-		return sb.toString();
-	}
+    // Removes the i-th element and returns its value.
+    // Decrements the size of the list by one.
+    public E remove(int i) {
+        if (i < 0 || i >= nodeCount) {
+            throw new IndexOutOfBoundsException();
+        }
+        E temp = head.getValue();
+        if (i == 0) {
+            head = head.getNext();
+            if (head == null) {
+                tail = null;
+            }
+        } else {
+            ListNode<E> temp2 = forHelper(0, i - 1);
+            ListNode<E> temp3 = temp2.getNext();
+            temp = temp3.getValue();
+            temp2.setNext(temp3.getNext());
+            if (temp3 == tail) {
+                tail = temp2;
+            }
+        }
 
-	public ListNode<E> forHelper(int i, int n) {
-		ListNode<E> temp = head;
-		for (int j = 0; j < i - 1; j++) {
-			temp = temp.getNext();
-		}
-		return temp;
-	}
+        nodeCount--;
+        return (E) temp;
+    }
+
+    // Returns a string representation of this list exactly like that for
+    // MyArrayList.
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        E temp = (E) getHead();
+        while (temp != null) {
+            sb.append(((ListNode<E>) temp).getValue());
+            if (((ListNode<E>) temp).getNext() != null) {
+                sb.append(", ");
+            }
+            temp = (E) ((ListNode<E>) temp).getNext();
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    public ListNode<E> forHelper(int i, int n) {
+        ListNode<E> temp = head;
+        for (int j = 0; j < n; j++) {
+            if (temp == null) {
+                return null;
+            }
+            temp = temp.getNext();
+        }
+        return temp;
+    }
 }
--- Unit 2/SinglyLinkedListTester.java
+++ Unit 2/SinglyLinkedListTester.java
@@ -1,3 +1,66 @@
+
 public class SinglyLinkedListTester {
 
+    public static void main(String[] args) {
+        SinglyLinkedList<String> list = new SinglyLinkedList<>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        System.out.println(list.getHead().getValue()); // A
+        System.out.println(list.getTail().getValue()); // C
+        System.out.println(list); // [A,B,C]
+        list.add(1, "X");
+        System.out.println(list); // [A,X,B,C]
+        list.remove(2);
+        System.out.println(list); // [A,X,C]
+        System.out.println(list.get(1)); // X
+        list.set(1, "Y");
+        System.out.println(list); // [A,Y,C]
+        System.out.println(list.isEmpty()); // false
+        System.out.println(list.size()); // 3
+        list.remove("A");
+        System.out.println(list); // [Y,C]
+        System.out.println(list.indexOf("C")); // 1
+        System.out.println(list.indexOf("Z")); // -1
+        System.out.println(list.contains("Y")); // true
+        System.out.println(list.contains("A")); // false
+
+
+        list = new SinglyLinkedList<>();
+        System.out.println(list.isEmpty()); // true
+        System.out.println(list.size()); // 0
+        System.out.println(list); // []
+        list.add("M");
+        list.add("N");
+        list.add("O");
+        System.out.println(list); // [M,N,O]
+        System.out.println(list.getHead().getValue()); // M
+        System.out.println(list.getTail().getValue()); // O
+        list.remove(0);
+        System.out.println(list); // [N,O]
+        list.remove(list.size() - 1);
+        System.out.println(list); // [N]
+        list.add("P");
+        System.out.println(list); // [N,P]
+        list.add(0, "Q");
+        System.out.println(list); // [Q,N,P]
+        list.set(2, "R");
+        System.out.println(list); // [Q,N,R]
+        System.out.println(list.get(2)); // R
+        list.add(1, "S");
+        System.out.println(list); // [Q,S,N,R]
+        list.remove("S");
+        System.out.println(list); // [Q,N,R]
+
+        list = new SinglyLinkedList<>();
+        System.out.println(list); // []
+        System.out.println(list.isEmpty()); // true
+        list.add("T");
+        System.out.println(list.getHead().getValue()); // T
+        System.out.println(list.getTail().getValue()); // T
+        System.out.println(list.size()); // 1
+        list.remove(0);
+        System.out.println(list); // []
+        System.out.println(list.size()); // 0
+    }
 }
