5d7a69dc9ab804ccaf33f1c247e2bd85d68ec098
mattin-tasbihgoo <mtasbihgoo1@hwemail.com>
2025-10-20 11:17:02 -0700

    20 October 2025
    Sorry I was sleepy eepy

---
+++ Unit 3/Recursion/RecursionTester.java
@@ -0,0 +1,13 @@
+public class RecursionTester {
+    public static void main(String[] args) {
+        ListNode third = new ListNode("object");
+        ListNode second = new ListNode("whaddup", third);
+        ListNode head = new ListNode("gimme head", second);
+
+        String[][] grid = new String[][] { { "a" }, { "a" } };
+    }
+
+    private void gridFucntion(String[][] grid) {
+
+    }
+}
