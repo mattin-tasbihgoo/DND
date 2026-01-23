d4ffa8ec827ae67df00c03d0f4f1e64777330e3f
mattin-tasbihgoo <mtasbihgoo1@hwemail.com>
2026-01-23 08:32:49 -0800

    Beginning of Class 23 January 2026
    I was having trouble booting into my computer. I was sick last class

---
+++ Unit 4/Stack/Arithmetic.java
@@ -0,0 +1,25 @@
+
+public class Arithmetic {
+
+	//Evaluates a String exp that has an arithmetic expression, written in classic notation
+	public static int evaluate(String exp) {
+		return 0;
+	}
+	
+	//Returns the result of doing operand1 operation operand2,
+	//e.g. operate(5, 2, "-") should return 3
+	public static int operate(int operand1, int operand2, String operation) {
+		return 0;
+	}
+	
+	//Evaluates a String exp that has an arithmetic expression written in STOUT notation
+	public static int evaluateStout(String exp) {
+		return 0;
+	}
+	
+	public static String convertClassicToStout(String exp) {
+		return "";
+	}
+	
+	
+}
