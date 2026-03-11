0e34b33d1eaa7a0203f1d86eb098b57211c1e337
mattin-tasbihgoo <mtasbihgoo1@hwemail.com>
2026-03-11 08:43:06 -0700

    Refactored/organized

---
+++ Unit 6/MiniGPT/MiniGPT.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MiniGPT {
+
+	public MiniGPT(String fileName, int chainOrder) {
+
+	}
+
+	
+	public void generateText(String outputFileName, int numChars) {
+		
+	}
+}
